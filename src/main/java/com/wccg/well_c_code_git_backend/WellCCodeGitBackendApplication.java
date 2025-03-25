08ea28b7ebbbad2b6d992a562fe1b0db5ec0918c
kangsan <devstar1223@gmail.com>
2025-03-25 13:54:17 +0900

    initial commit

---
+++ src/main/java/com/wccg/well_c_code_git_backend/WellCCodeGitBackendApplication.java
@@ -0,0 +1,13 @@
+package com.wccg.well_c_code_git_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WellCCodeGitBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WellCCodeGitBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/wccg/well_c_code_git_backend/WellCCodeGitBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wccg.well_c_code_git_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WellCCodeGitBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
