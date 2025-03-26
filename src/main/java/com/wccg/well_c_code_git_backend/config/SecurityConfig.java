372ec88dd36a9154f00a8c6a1423daf0377bfe66
kangsan <devstar1223@gmail.com>
2025-03-26 11:27:51 +0900

    🔧 chore: 스프링 시큐리티 설정 H2 콘솔 iframe 차단 해제 (개발용 설정)

--- src/main/java/com/wccg/well_c_code_git_backend/config/SecurityConfig.java
+++ src/main/java/com/wccg/well_c_code_git_backend/config/SecurityConfig.java
@@ -5,6 +5,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.annotation.web.configurers.HeadersConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -17,6 +18,7 @@ public class SecurityConfig {
                 .authorizeHttpRequests(auth -> auth
                         .anyRequest().permitAll() // 모든 API 인증 없이 개방(초기 개발 설정)
                 )
+                .headers(headers -> headers.frameOptions(HeadersConfigurer.FrameOptionsConfig::disable))
                 .formLogin(AbstractHttpConfigurer::disable) // 기본 로그인 비활성화
                 .httpBasic(AbstractHttpConfigurer::disable); // HTTP Basic 인증 비활성화
         return http.build();
