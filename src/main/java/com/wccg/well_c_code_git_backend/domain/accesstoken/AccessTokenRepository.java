129a0f46ecf01eec78ecb8c8c6e1eac3bea1837d
kangsan <devstar1223@gmail.com>
2025-03-28 13:55:13 +0900

    ✨ feat(AccessTokenRepository): JPA 리포지토리 추가

---
+++ src/main/java/com/wccg/well_c_code_git_backend/domain/accesstoken/AccessTokenRepository.java
@@ -0,0 +1,7 @@
+package com.wccg.well_c_code_git_backend.domain.accesstoken;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface AccessTokenRepository extends JpaRepository<AccessToken, Long> {
+
+}
