dbecee983c89008ecab44b187a4f19ad0d6efaa3
kangsan <devstar1223@gmail.com>
2025-03-28 13:56:11 +0900

    ✨ feat(WccgRepositoryRepository): JPA 리포지토리 추가

---
+++ src/main/java/com/wccg/well_c_code_git_backend/domain/wccgrepository/WccgRepositoryRepository.java
@@ -0,0 +1,8 @@
+package com.wccg.well_c_code_git_backend.domain.wccgrepository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface WccgRepositoryRepository extends JpaRepository<WccgRepository, Long> {
+
+}
+
