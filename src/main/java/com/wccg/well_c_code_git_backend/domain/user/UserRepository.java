aeb2295b8b6f0620d4a7c3a404d93059ce613a7a
kangsan <devstar1223@gmail.com>
2025-03-28 13:53:51 +0900

    ✨ feat(UserRepository): JPA 리포지토리 추가

---
+++ src/main/java/com/wccg/well_c_code_git_backend/domain/user/UserRepository.java
@@ -0,0 +1,7 @@
+package com.wccg.well_c_code_git_backend.domain.user;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+
+}
