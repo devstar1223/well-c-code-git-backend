e1d5b2237e3928ad60e2f4be2aa7a3f03beac1f4
kangsan <devstar1223@gmail.com>
2025-03-29 12:06:56 +0900

    ♻️ refactor(GithubOAuthProperties): scope 설정 프로퍼티 주입

--- src/main/java/com/wccg/well_c_code_git_backend/config/GithubOAuthProperties.java
+++ src/main/java/com/wccg/well_c_code_git_backend/config/GithubOAuthProperties.java
@@ -1,14 +1,17 @@
 package com.wccg.well_c_code_git_backend.config;
 
 import lombok.Getter;
+import lombok.Setter;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.stereotype.Component;
 
 @Component
 @ConfigurationProperties(prefix = "github")
 @Getter
+@Setter
 public class GithubOAuthProperties {
     private String clientId;
     private String clientSecret;
     private String redirectUri;
+    private String scope;
 }
