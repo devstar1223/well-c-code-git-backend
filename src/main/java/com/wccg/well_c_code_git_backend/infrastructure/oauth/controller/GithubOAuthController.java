85361a6bbf60c1e1fa26e689869830dff81b6c67
kangsan <devstar1223@gmail.com>
2025-03-29 17:18:58 +0900

    ✨ feat(GithubOAuthController): GitHub OAuth 콜백및 데이터 저장 API 엔드포인트 구현

--- src/main/java/com/wccg/well_c_code_git_backend/infrastructure/oauth/controller/GithubOAuthController.java
+++ src/main/java/com/wccg/well_c_code_git_backend/infrastructure/oauth/controller/GithubOAuthController.java
@@ -7,6 +7,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -21,5 +22,13 @@ public class GithubOAuthController {
         return ResponseEntity
                 .status(HttpStatus.OK)
                 .body(githubOAuthService.generateLoginUrl());
+    }
+
+    @GetMapping("/callback/github")
+    public ResponseEntity<String> githubCallback(@RequestParam("code") String code) {
+        githubOAuthService.processGithubCallback(code);
+        return ResponseEntity
+                .status(HttpStatus.OK)
+                .body("Github 로그인에 성공 했습니다.");
     }
 }
