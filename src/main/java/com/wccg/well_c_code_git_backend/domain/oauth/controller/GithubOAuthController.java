dd1db9afb002ff8d761ec8657c05b869752b8c27
kangsan <devstar1223@gmail.com>
2025-03-29 12:37:52 +0900

    ✨ feat(GithubOAuthController): OAuth2 로그인 API 컨트롤러 추가

---
+++ src/main/java/com/wccg/well_c_code_git_backend/domain/oauth/controller/GithubOAuthController.java
@@ -0,0 +1,25 @@
+package com.wccg.well_c_code_git_backend.domain.oauth.controller;
+
+import com.wccg.well_c_code_git_backend.domain.oauth.service.GithubOAuthService;
+import com.wccg.well_c_code_git_backend.domain.oauth.dto.GithubLoginUrlResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/oauth")
+public class GithubOAuthController {
+
+    private final GithubOAuthService githubOAuthService;
+
+    @GetMapping("/login/github")
+    public ResponseEntity<GithubLoginUrlResponse> getGithubLoginUrl() {
+        return ResponseEntity
+                .status(HttpStatus.OK)
+                .body(githubOAuthService.generateLoginUrl());
+    }
+}
