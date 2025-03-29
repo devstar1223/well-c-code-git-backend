e1d76ce65e96551459a24cc9da8bbef662ff1e34
kangsan <devstar1223@gmail.com>
2025-03-29 12:39:14 +0900

    ✨ feat(GithubLoginUrlResponse): 로그인 URL DTO 생성

---
+++ src/main/java/com/wccg/well_c_code_git_backend/domain/oauth/dto/GithubLoginUrlResponse.java
@@ -0,0 +1,10 @@
+package com.wccg.well_c_code_git_backend.domain.oauth.dto;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
+public class GithubLoginUrlResponse {
+    private final String loginUrl;
+}
