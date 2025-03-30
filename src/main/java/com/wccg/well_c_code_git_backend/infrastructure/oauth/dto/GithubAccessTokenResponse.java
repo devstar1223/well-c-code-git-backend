35e051734815241d219cead7416316bfdaf903dc
kangsan <devstar1223@gmail.com>
2025-03-30 17:52:13 +0900

    ♻️ refactor(GithubResponse): OAuth 응답 DTO를 불변 객체로 리팩토링

--- src/main/java/com/wccg/well_c_code_git_backend/infrastructure/oauth/dto/GithubAccessTokenResponse.java
+++ src/main/java/com/wccg/well_c_code_git_backend/infrastructure/oauth/dto/GithubAccessTokenResponse.java
@@ -1,10 +1,14 @@
 package com.wccg.well_c_code_git_backend.infrastructure.oauth.dto;
 
+import com.fasterxml.jackson.annotation.JsonCreator;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Getter;
+import lombok.RequiredArgsConstructor;
 
 @Getter
+@RequiredArgsConstructor(onConstructor_ = @JsonCreator)
 public class GithubAccessTokenResponse {
+
     @JsonProperty("access_token")
-    private String accessToken;
+    private final String accessToken;
 }
--- src/main/java/com/wccg/well_c_code_git_backend/infrastructure/oauth/dto/GithubUserResponse.java
+++ src/main/java/com/wccg/well_c_code_git_backend/infrastructure/oauth/dto/GithubUserResponse.java
@@ -1,82 +1,85 @@
 package com.wccg.well_c_code_git_backend.infrastructure.oauth.dto;
 
+import com.fasterxml.jackson.annotation.JsonCreator;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Getter;
+import lombok.RequiredArgsConstructor;
 
 @Getter
+@RequiredArgsConstructor(onConstructor_ = @JsonCreator)
 public class GithubUserResponse {
 
-    private String login;
-    private Long id;
+    private final String login;
+    private final Long id;
 
     @JsonProperty("node_id")
-    private String nodeId;
+    private final String nodeId;
 
     @JsonProperty("avatar_url")
-    private String avatarUrl;
+    private final String avatarUrl;
 
     @JsonProperty("gravatar_id")
-    private String gravatarId;
+    private final String gravatarId;
 
-    private String url;
+    private final String url;
 
     @JsonProperty("html_url")
-    private String htmlUrl;
+    private final String htmlUrl;
 
     @JsonProperty("followers_url")
-    private String followersUrl;
+    private final String followersUrl;
 
     @JsonProperty("following_url")
-    private String followingUrl;
+    private final String followingUrl;
 
     @JsonProperty("gists_url")
-    private String gistsUrl;
+    private final String gistsUrl;
 
     @JsonProperty("starred_url")
-    private String starredUrl;
+    private final String starredUrl;
 
     @JsonProperty("subscriptions_url")
-    private String subscriptionsUrl;
+    private final String subscriptionsUrl;
 
     @JsonProperty("organizations_url")
-    private String organizationsUrl;
+    private final String organizationsUrl;
 
     @JsonProperty("repos_url")
-    private String reposUrl;
+    private final String reposUrl;
 
     @JsonProperty("events_url")
-    private String eventsUrl;
+    private final String eventsUrl;
 
     @JsonProperty("received_events_url")
-    private String receivedEventsUrl;
+    private final String receivedEventsUrl;
 
-    private String type;
+    private final String type;
 
     @JsonProperty("site_admin")
-    private boolean siteAdmin;
+    private final boolean siteAdmin;
 
-    private String name;
-    private String company;
-    private String blog;
-    private String location;
-    private String email;
-    private String bio;
+    private final String name;
+    private final String company;
+    private final String blog;
+    private final String location;
+    private final String email;
+    private final String bio;
 
     @JsonProperty("twitter_username")
-    private String twitterUsername;
+    private final String twitterUsername;
 
     @JsonProperty("public_repos")
-    private int publicRepos;
+    private final int publicRepos;
 
     @JsonProperty("public_gists")
-    private int publicGists;
+    private final int publicGists;
 
-    private int followers;
-    private int following;
+    private final int followers;
+    private final int following;
 
     @JsonProperty("created_at")
-    private String createdAt;
+    private final String createdAt;
 
     @JsonProperty("updated_at")
-    private String updatedAt;
+    private final String updatedAt;
 }
