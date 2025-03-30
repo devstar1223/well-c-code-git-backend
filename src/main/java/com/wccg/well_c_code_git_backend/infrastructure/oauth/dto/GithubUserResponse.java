39661a17f966e4ba25dc15578efa489385d0ddab
kangsan <devstar1223@gmail.com>
2025-03-30 17:20:10 +0900

    ♻️ refactor(GithubUserResponse): 외부 API 사용자 정보 필드 확장

--- src/main/java/com/wccg/well_c_code_git_backend/infrastructure/oauth/dto/GithubUserResponse.java
+++ src/main/java/com/wccg/well_c_code_git_backend/infrastructure/oauth/dto/GithubUserResponse.java
@@ -7,11 +7,76 @@ import lombok.Getter;
 public class GithubUserResponse {
 
     private String login;
+    private Long id;
 
-    private String name;
-
-    private String bio;
+    @JsonProperty("node_id")
+    private String nodeId;
 
     @JsonProperty("avatar_url")
     private String avatarUrl;
+
+    @JsonProperty("gravatar_id")
+    private String gravatarId;
+
+    private String url;
+
+    @JsonProperty("html_url")
+    private String htmlUrl;
+
+    @JsonProperty("followers_url")
+    private String followersUrl;
+
+    @JsonProperty("following_url")
+    private String followingUrl;
+
+    @JsonProperty("gists_url")
+    private String gistsUrl;
+
+    @JsonProperty("starred_url")
+    private String starredUrl;
+
+    @JsonProperty("subscriptions_url")
+    private String subscriptionsUrl;
+
+    @JsonProperty("organizations_url")
+    private String organizationsUrl;
+
+    @JsonProperty("repos_url")
+    private String reposUrl;
+
+    @JsonProperty("events_url")
+    private String eventsUrl;
+
+    @JsonProperty("received_events_url")
+    private String receivedEventsUrl;
+
+    private String type;
+
+    @JsonProperty("site_admin")
+    private boolean siteAdmin;
+
+    private String name;
+    private String company;
+    private String blog;
+    private String location;
+    private String email;
+    private String bio;
+
+    @JsonProperty("twitter_username")
+    private String twitterUsername;
+
+    @JsonProperty("public_repos")
+    private int publicRepos;
+
+    @JsonProperty("public_gists")
+    private int publicGists;
+
+    private int followers;
+    private int following;
+
+    @JsonProperty("created_at")
+    private String createdAt;
+
+    @JsonProperty("updated_at")
+    private String updatedAt;
 }
