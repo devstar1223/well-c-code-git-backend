1db8242e19ef607bc82e63e58973f42cc87f00cb
kangsan <devstar1223@gmail.com>
2025-03-31 14:23:44 +0900

    ✨ feat(JwtProvider): JWT 생성, 검증, ID 추출 기능 추가

---
+++ src/main/java/com/wccg/well_c_code_git_backend/infrastructure/jwt/JwtProvider.java
@@ -0,0 +1,57 @@
+package com.wccg.well_c_code_git_backend.infrastructure.jwt;
+
+import com.wccg.well_c_code_git_backend.domain.user.UserRole;
+import io.jsonwebtoken.*;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+import java.security.Key;
+import java.util.Date;
+
+@Component
+@RequiredArgsConstructor
+public class JwtProvider {
+
+    private Key secretKey;
+    private long jwtAccessTokenValidityInMillis;
+
+    public JwtProvider(JwtProperties jwtProperties) {
+        this.secretKey = jwtProperties.getSecretKey();
+        this.jwtAccessTokenValidityInMillis = jwtProperties.getJwtAccessTokenValidityInMillis();
+    }
+
+    public String createToken(Long userId) {
+        Date now = new Date();
+        Date expiry = new Date(now.getTime() + jwtAccessTokenValidityInMillis);
+
+        return Jwts.builder()
+                .setSubject(String.valueOf(userId)) // 유저 ID를 subject에 저장
+                .setIssuedAt(now)
+                .setExpiration(expiry)
+                .claim("role", UserRole.USER)
+                .signWith(secretKey, SignatureAlgorithm.HS256)
+                .compact();
+    }
+
+    public boolean validateToken(String token) {
+        try {
+            Jwts.parserBuilder()
+                    .setSigningKey(secretKey)
+                    .build()
+                    .parseClaimsJws(token);
+            return true;
+        } catch (JwtException | IllegalArgumentException e) {
+            return false;
+        }
+    }
+
+    public Long getUserId(String token) {
+        return Long.valueOf(
+                Jwts.parserBuilder()
+                        .setSigningKey(secretKey)
+                        .build()
+                        .parseClaimsJws(token)
+                        .getBody()
+                        .getSubject()
+        );
+    }
+}
